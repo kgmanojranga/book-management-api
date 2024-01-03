be1bd00f4beb0180007f07273baa1a755475d241
Manoj Ranga <manojranga7583@gmail.com>
2024-01-03 15:25:47 +0530

    feat: implement update endpoint

--- src/routes/book-route.ts
+++ src/routes/book-route.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import {createBook, getAllBooks, getBook} from "../controllers/book-controllers";
+import {createBook, getAllBooks, getBook, updateBook} from "../controllers/book-controllers";
 
 const router = express.Router();
 
 router.route('/').post(createBook).get(getAllBooks);
-router.route("/:id").get(getBook);
+router.route("/:id").get(getBook).patch(updateBook);
 
 export default router;

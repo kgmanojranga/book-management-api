69326c2689d8e94bb9a92bbc0cbda60cfe7dd93c
Manoj Ranga <manojranga7583@gmail.com>
2024-01-03 13:06:39 +0530

    feat: initializing book model queries

--- src/models/bookModel.ts
+++ src/models/bookModel.ts
@@ -17,3 +17,21 @@ const bookSchema = new mongoose.Schema({
 
 
 export const BookModel = mongoose.model("Book", bookSchema);
+
+// Create a book on database
+export const createBooks = (values: Record<string, any>) => new BookModel(values).save();
+
+// Get all book from database
+export const getBooks = () => BookModel.find();
+
+// Get a book using book id
+export const getBookById = (id: string) => BookModel.findById(id);
+
+// Update a book using book id
+
+export const updateBookById = (id: string, values: Record<string, any>) => BookModel.findByIdAndUpdate(id, values, {
+    new: true,
+    runValidators: true
+})
+
+export const deleteBookById = (id: string) => BookModel.findByIdAndDelete(id);

ef8fbcba2a7cd2580815552a1bdf20cfe753db6b
Manoj Ranga <manojranga7583@gmail.com>
2024-01-03 12:56:54 +0530

    feat: initializing book schema and model

---
+++ src/models/bookModel.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+});
+
+
+export const BookModel = mongoose.model("Book", bookSchema);
